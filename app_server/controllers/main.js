86f3767551ed2ab0ea6be84fa29718c23be46da0
Drew Sibila <drew21505@gmail.com>
2026-05-24 21:54:31 -0500

    Completed Module Three dynamic JSON rendering

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,9 +1,19 @@
+const trips = require('../data/trips.json');
+
 const index = (req, res) => {
     res.render('index', {
         title: 'Travlr Getaways'
     });
 };
 
+const travel = (req, res) => {
+    res.render('travel', {
+        title: 'Travel',
+        trips
+    });
+};
+
 module.exports = {
-    index
+    index,
+    travel
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const ctrlMain = require('../controllers/main');
 
 router.get('/', ctrlMain.index);
+router.get('/travel', ctrlMain.travel);
 
 module.exports = router;
